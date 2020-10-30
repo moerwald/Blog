fd51ba16ffb7170f2d6407fed9a1a5c7264f2ef6
moerwald <andreas.mewald@gmail.com>
2020-10-30 17:34:09 +0100

    Added repository interface to decouple controllers from DbContext.

---
+++ src/Blog/Blog/Data/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Blog.Data
+{
+    public interface IRepository
+    {
+        void Add(Post post);
+        void Update(Post post);
+        void Remove(Post post);
+        void Remove(int id);
+        Task<Post> GetAsync(int id);
+        Task<IReadOnlyList<Post>> GetAllAsync();
+        Task<bool> SaveAsync();
+    }
+}
---
+++ src/Blog/Blog/Data/RepositoryForPosts.cs
@@ -0,0 +1,33 @@
+﻿using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Data
+{
+    public class RepositoryForPosts : IRepository
+    {
+        private readonly AppDbContext _ctx;
+
+        public RepositoryForPosts(AppDbContext ctx) => _ctx = ctx;
+
+        public void Add(Post post) => _ctx.Add(post);
+
+        public Task<Post> GetAsync(int id) => _ctx.Posts.SingleAsync(p => p.Id.Equals(id));
+
+        public async Task<IReadOnlyList<Post>> GetAllAsync() => (await _ctx.Posts.ToListAsync()).AsReadOnly();
+
+        public void Remove(Post post) => _ctx.Remove(post);
+
+        public void Remove(int id)
+        {
+            var x = _ctx.Posts.Single(p => p.Id.Equals(id));
+            _ctx.Posts.Remove(x);
+        }
+
+        public async Task<bool> SaveAsync() => (await _ctx.SaveChangesAsync()) > 0;
+
+        public void Update(Post post) => _ctx.Posts.Update(post);
+    }
+}
