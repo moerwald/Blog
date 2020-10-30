6c5d812c1e85c57cd739df4e5bb491f8b861455d
moerwald <andreas.mewald@gmail.com>
2020-10-30 21:10:56 +0100

    Removed useless link

--- src/Blog/Blog/Controllers/HomeController.cs
+++ src/Blog/Blog/Controllers/HomeController.cs
@@ -1,39 +1,40 @@
 ﻿using Blog.Data;
 using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace Blog.Controllers
 {
-    public class HomeController: Controller
+    public class HomeController : Controller
     {
         private readonly IRepository _repo;
+        private static Post _nullPost = new Post();
 
         public HomeController(IRepository repo) => _repo = repo;
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var repos = await _repo.GetAllAsync();
+            return View(repos);
         }
 
-        public IActionResult Post()
+        public async Task<IActionResult> Post(int id)
         {
-            return View();
+            var post = await _repo.GetAsync(id);
+            return View(post);
         }
 
         [HttpGet]
-        public IActionResult Edit()
-        {
-            return View(new Post());
-        }
+        public async Task<IActionResult> Edit(int? id) => View(id.HasValue? await _repo.GetAsync(id.Value) : _nullPost);
 
         [HttpPost]
         public async Task<IActionResult> Edit(Post post)
         {
-            _repo.Add(post);
+            ((post?.Id.Equals(0) ?? false) ? (Action<Post>)_repo.Add : _repo.Update).Invoke(post);
             await _repo.SaveAsync();
-            return Redirect(nameof(Index));
+            return RedirectToAction(nameof(Index));
         }
-        
+
     }
 }
